82d661bd1a92b47d176e4dfbcdfbb72f6fd11d6f
Lee Mercer <80193271+Blankstate1999@users.noreply.github.com>
2021-09-26 09:56:58 +0100

    Create install.R

---
+++ install.R
@@ -0,0 +1,1 @@
+install.packages(c("see", "tidyverse", "ggrepel", "patchwork", "dplyr", "ggplot2", "afex", "emmeans", "Hmisc", "ggridges", "NHANES", "performance", "MASS", "car", "olsrr", "visdat"))
